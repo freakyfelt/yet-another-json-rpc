c31dc89ac95b27789e1d071b7bf03b228ef3cc33
Bruce Felt <freakyfelt@gmail.com>
2023-08-13 17:30:42 +0200

    chore: drop monorepo structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,23 @@
 /* eslint-env node */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-  ],
-  env: {
-    es2021: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-    project: ['./tsconfig.json', './packages/*/tsconfig.json'],
-  },
-  plugins: ['@typescript-eslint'],
-  root: true,
-  rules: {
-    '@typescript-eslint/no-floating-promises': 'off'
-  }
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking",
+		"prettier",
+	],
+	env: {
+		es2021: true,
+	},
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		sourceType: "module",
+		project: ["./tsconfig.json", "./packages/*/tsconfig.json"],
+	},
+	plugins: ["@typescript-eslint"],
+	root: true,
+	rules: {
+		"@typescript-eslint/no-floating-promises": "off",
+	},
 };
